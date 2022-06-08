ae1be539a43ed1b2045184c7bd1aaf9410b70470
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-06-08 12:57:49 +0200

    imported svg. Working on lobby data.

--- src/Components/Lobby/Lobby.jsx
+++ src/Components/Lobby/Lobby.jsx
@@ -1,13 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import './Lobby.css';
 
+import {ReactComponent as PrivateSvg} from '../../assets/svg/private.svg'
+import {ReactComponent as PlayersSvg} from '../../assets/svg/players.svg'
+import {ReactComponent as JoinBtnSvg} from '../../assets/svg/join.svg'
+import {ReactComponent as EyeSvg} from '../../assets/svg/eye.svg'
+
 const Lobby = (props) => {
 
 
 
   return (
     <div className="box_lobby_card">
+      <div className="lobby_data centered_content">
 
+      </div>
+      <div className="lobby_data centered_content">
+        <PlayersSvg/>
+        
+      </div>
+      <div className="lobby_data centered_content">
+        
+      </div>
+      <div className="lobby_data centered_content">
+        
+      </div>
     </div>
   )
 }
